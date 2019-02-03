d6fb6b3f7a257a6a4ba820e446d5c0b0612f8a39
Chance Smith <chancesmith97@hotmail.com>
2019-02-03 23:17:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -15,7 +15,6 @@ def using_pop(x)
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   x = great_hits_of_the_nineties
   x.pop 
-  return x 
 end
 
 def pop_with_args(x)
