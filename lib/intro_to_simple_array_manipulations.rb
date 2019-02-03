a49e73d930a3c25ebe1b08907786feed5a007a9a
Chance Smith <chancesmith97@hotmail.com>
2019-02-03 23:11:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -14,7 +14,7 @@ def using_pop(x)
   
   great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   x = great_hits_of_the_nineties
-  x.pop
+  x.pop = x.size 
   
 
   
