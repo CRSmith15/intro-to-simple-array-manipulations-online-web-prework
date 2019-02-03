ae182d62c29a0b78686eb59efd947726be7a4c34
Chance Smith <chancesmith97@hotmail.com>
2019-02-03 23:29:04 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -11,8 +11,6 @@ def using_unshift (x, y)
 end 
 
 def using_pop(x)
-  
-  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
   x = great_hits_of_the_nineties
   x.pop 
 end
