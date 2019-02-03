c306fb30058c98bd66ba85978880b42fb01dbea7
Chance Smith <chancesmith97@hotmail.com>
2019-02-03 23:07:47 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_simple_array_manipulations.rb
@@ -0,0 +1,21 @@
+def using_push(x, y)
+  x = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+  y = "Niger"
+  updated_array = x.push(y)
+end 
+
+def using_unshift (x, y)
+  x = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+  y = "Brooklyn Heights"
+  updated_array = x.unshift(y)
+end 
+
+def using_pop(x)
+  
+  great_hits_of_the_nineties = ["Baby One More Time", "Smells Like Teen Spirit", "Missing", "Walking On The Sun", "Hard Knock Life", "Losing My Religion"]
+  x = great_hits_of_the_nineties
+  x.pop
+  x.size 
+
+  
+end
